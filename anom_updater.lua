9b656c981553612925e1bd3a19a05f6de4b364f7
nexus331 <memelordyt53@gmail.com>
2026-06-12 11:10:42 +0800

    Major Installer Update

--- anom_updater.lua
+++ anom_updater.lua
@@ -1,5 +1,23 @@
 -- ANOMALY ALERT SYSTEM SOFTWARE UPDATER
 
+function anom_installer()
+    print("Welcome to the Anomaly Alert System Software Installer")
+    print("Please select the software you wish to install...")
+    print("1. ALS Server")
+    print("2. ALS Client")
+    io.write("Enter option:")
+    option = io.read()
+
+    if option == "1" then
+        textutils.slowPrint("Installing ALS Server...")
+        shell.run("wget https://raw.githubusercontent.com/nexus331/CCNexus-Projects/refs/heads/main/anomaly_server.lua")
+    elseif option == "2" then
+        textutils.slowPrint("Installing ALS Client...")
+        shell.run("wget https://raw.githubusercontent.com/nexus331/CCNexus-Projects/refs/heads/main/anomaly_alert.lua")
+    end
+    
+end
+
 function update_anom()
     print("Initiating System Update....")
     if fs.exists("anomaly_server.lua") then
@@ -14,6 +32,9 @@ function update_anom()
         shell.run("wget https://raw.githubusercontent.com/nexus331/CCNexus-Projects/refs/heads/main/anomaly_alert.lua")
         sleep(2)
         shell.run("reboot")
+    else
+        print("No existing installation detected...")
+        anom_installer()
     end
 end
 
